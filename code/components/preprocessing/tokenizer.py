6ef1de456ab5429874378280100dd203d7339121
Marc Verhagen <marc@cs.brandeis.edu>
2010-09-21 03:57:56 +0000

    More changes to tokenizer.

--- code/components/preprocessing/tokenizer.py
+++ code/components/preprocessing/tokenizer.py
@@ -55,7 +55,8 @@ split_abbrev_pattern = re.compile(r'(\S+) \.')
 eos_pattern = re.compile(r' [.!?]( [")])?$')
 
 # punctuation patterns
-punctuation_pattern = re.compile(r'([.,!?\'\`\";:]|[\]\[\(\){}])')
+punctuation_pattern = re.compile(r'([.,!?\'\`\";:]|[\]\[\(\){}]\<\>)')
+punctuation_pattern = re.compile(r'[.,!?\'\`\";:\]\[\(\){}\<\>]')
 final_punct1_pattern = re.compile(r'(([.,!?\'\`\";:]|[\]\[\(\){}])$)')
 final_punct2_pattern = re.compile(r'(([.,!?\'\`\";:]|[\]\[\(\){}]) .$)')
 final_punct3_pattern = re.compile(r'(([.,!?\'\`\";:]|[\]\[\(\){}]) . .$)')
@@ -168,14 +169,31 @@ def tokenize_string(string, format='text'):
 
 
 
+#### NEW STUFF
 
-def test_space(char):
+# TODO: add abbrev patter for initials and acronyms
+
+punctuation_pattern = re.compile(r'[.,!?\'\`\";:\]\[\(\){}\<\>]')
+
+t_pattern = re.compile(r"(\w+)'t ", re.IGNORECASE)
+d_pattern = re.compile(r"(\w+)'d ", re.IGNORECASE)
+s_pattern = re.compile(r"(\w+)'s ", re.IGNORECASE)
+m_pattern = re.compile(r"(\w+)'m ", re.IGNORECASE)
+re_pattern = re.compile(r"(\w+)'re ", re.IGNORECASE)
+ll_pattern = re.compile(r"(\w+)'ll ", re.IGNORECASE)
+ve_pattern = re.compile(r"(\w+)'ve ", re.IGNORECASE)
+posse_pattern = re.compile(r"(\S+)'s ", re.IGNORECASE)
+
+contraction_pattern = re.compile(r"(\w+)'(t|d|s|m|re|ll|ve|s)$", re.IGNORECASE)
+
+
+def _test_space(char):
     return char.isspace()
 
-def test_nonspace(char):
+def _test_nonspace(char):
     return not char.isspace()
         
-def slurp(string, offset, test):
+def _slurp(string, offset, test):
     begin = offset
     end = offset
     length = len(string)
@@ -188,71 +206,111 @@ def slurp(string, offset, test):
             return (begin, end, string[begin:end])
     return (begin, end, string[begin:end])
 
-
-
 def slurp_token(string, offset):
-
+    """Given a string and an offset in the string, return two tuples, one for whitespace after
+    the offset and one for a sequence of non-whitespace immdediately after the whitespace. A
+    tuple consists of an begin offset, an end offset and a string."""
     begin = offset
     end = offset
-    (o1, o2, space) = slurp(string, offset, test_space)
-    (o3, o4, token) = slurp(string, o2, test_nonspace)
+    (o1, o2, space) = _slurp(string, offset, _test_space)
+    (o3, o4, token) = _slurp(string, o2, _test_nonspace)
     return ((o1, o2, space), (o3, o4, token))
 
 
+def tokenize_text(text):
 
-
-def tokenize_string_new(string, format='text'):
-
-    """Tokenize a string and return it in a one-sentence-per-line format with tokens
-    separated by a space.
-
-    arguments:
-       string - the string to be tokenized
-       format - determines output format, choice of text and xml
-                (not yet used)
-    returns:
-       a tokenized string in text or xml format."""
+    """Tokenize a text and return a set of tokens and a set of sentences. Each token and each
+    sentence is a pair of a begin position and end position."""
 
     offset = 0
-    length = len(string)
+    length = len(text)
 
     all_tokens = []
     while offset < length:
-        (space, token) = slurp_token(string, offset)
-        if token[2]:
-            tokens = X_tokenize(token, string)
-            all_tokens += tokens
-        offset = token[1]
+        (space, word) = slurp_token(text, offset)
+        if word[2]:
+            tokens = _split_word(word, text)
+            all_tokens.append(tokens)
+        offset = word[1]
 
-    return all_tokens
-
-
-
-
-def X_tokenize(token, string):
-
-    (opening_puncts, core_token, closing_puncts) = X_split_punctuation(token)
-
-    if closing_puncts and closing_puncts[0][2] == '.':
-        print ':::', core_token
+    _split_contractions(all_tokens)
         
-    return [token]
+    lexes = _get_lexes(all_tokens)
+    sentences = _get_sentences(all_tokens, text)
+    
+    return (sentences, lexes)
 
 
-def X_split_punctuation(token):
+
+def _get_lexes(all_tokens):
+    """Return all lexes in all_tokens as a flat list."""
+    all_lexes = []
+    for lexes in  [ p1 + [ct] + p2 for (p1, ct, p2) in all_tokens]:
+        all_lexes += lexes
+    return all_lexes
+
+
+def _get_sentences(all_tokens, text):
+
+    def is_sentence_final_abbrev(tok, puncts2, text):
+        if not puncts2 and tok[2] in dict_end_abbrevs:
+            (space, next_token) = slurp_token(text, tok[1])
+            return next_token[2] in dict_initial_tokens
+        return False
+
+    def is_eos(puncts2):
+        if puncts2:
+            for p in puncts2:
+                if p[2] in ('.', '?', '!'):
+                    return True
+        return False
+    
+    sentences = []
+    if not all_tokens:
+        return sentences
+    first = all_tokens[0][0][0][0]
+    for (puncts1, tok, puncts2) in all_tokens:
+        if first is None:
+            first = tok[0]
+        if is_sentence_final_abbrev(tok, puncts2, text) or is_eos(puncts2):
+            sentences.append( [first, tok[1]])
+            first = None
+    return sentences
+
+
+def _split_word(word, text):
+    """Split a word into it's constitutent parts. A word is a tuple of begin offset, end
+    offset and a sequence of non-whitespace characters."""
+    (opening_puncts, core_token, closing_puncts) = _split_punctuation(word)
+    if closing_puncts and closing_puncts[0][2] == '.':
+        (core_token, closing_puncts) = \
+            _restore_abbreviation(core_token, closing_puncts, text)
+    return (opening_puncts, core_token, closing_puncts)
+
+
+def _restore_abbreviation(core_token, closing_puncts, string):
+    """Glue the period back onto the core token if the first closing punctuation is a period
+    and the core token is a known abbreviation."""
+    last = closing_puncts[-1][1]
+    (space, next_token) = slurp_token(string, last)
+    if core_token[2]+'.' in dict_abbrevs:
+        core_token = (core_token[0], core_token[1] + 1, core_token[2] + '.')
+        closing_puncts.pop(0)
+    return (core_token, closing_puncts)
+
+
+def _split_punctuation(word):
 
     """Return a triple of opening punctuations, core token and closing punctuation. A core
     token can contain internal punctuation but token-initial and token-final punctuations
     are stripped off. If a token has punctuation characters only, then the core token wil
-    be '' and the closing list will be empty."""
+    be the empty string and the closing list will be empty."""
     
     opening_puncts = []
     closing_puncts = []
-    core_token = token
+    core_token = word
 
-    tok = token[2]
-    off1 = token[0]
-    off2 = token[1]
+    (off1, off2, tok) = word
 
     while True:
         if not tok: break
@@ -276,36 +334,23 @@ def X_split_punctuation(token):
         else:
             break
     
-    print opening_puncts + [core_token] + closing_puncts
-    #print (opening_puncts,  core_token, closing_puncts)
     return (opening_puncts,  core_token, closing_puncts)
 
 
+def _split_contractions(all_tokens):
 
-def X__something():
-    
-    tokenized_string = ''
-
-    for line in string.split("\n"):
-        line = line.strip()
-        if line.startswith('<P>'):
-            line = line[2:]
-            tokenized_string += '\n<P>\n\n'
-        tokens = line.split()  
-        index_count = 0
-        for token in tokens:
-            found_punc = punctuation_pattern.search(token)
-            if(found_punc):
-                token = Split_Punctuation(token)
-                token = Restore_Abbrevs(token)
-                token = Add_EOS_Marker(token, index_count, tokens)
-            else:
-                token = token + ' '
-            token = Split_Contractions(token)
-            tokenized_string += token
-            index_count += 1
-
-    return tokenized_string
+    new_tokens = []
+    for (puncts1, tok, puncts2) in all_tokens:
+        found = contraction_pattern.search(tok[2])
+        if found:
+            idx = found.start(2) - 1
+            split_token = [(tok[0], tok[0] + idx, tok[2][:idx]),
+                           (tok[0] + idx, tok[1], tok[2][idx:])]
+            print split_token
+            new_tokens.append( (puncts1, split_token, puncts2) )
+        else:
+            new_tokens.append( (puncts1, tok, puncts2) )
+    return new_tokens
 
 
 if __name__ == '__main__':
@@ -313,6 +358,6 @@ if __name__ == '__main__':
     from time import time
     in_file = sys.argv[1]
     btime = time()
-    result = tokenize_string_new(open(in_file).read())
+    result = tokenize_text(open(in_file).read())
     #print result
     print "\nDONE, processing time was %.3f seconds\n" % (time() - btime)
