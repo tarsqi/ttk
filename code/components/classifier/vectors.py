73ac430b42ac197b6cfd136645d8b1702a85cfab
Marc Verhagen <marc@cs.brandeis.edu>
2016-03-08 17:42:19 -0500

    Updated the Classifier to work with the new design.

--- code/components/classifier/vectors.py
+++ code/components/classifier/vectors.py
@@ -1,5 +1,5 @@
 
-from library.timeMLspec import EVENT, MAKEINSTANCE, TIMEX, EID, EVENTID
+from library.timeMLspec import EVENT, TIMEX, EID, EVENTID
 
 # these should be defined in a library for classifier features
 C_TID = 'tid'
@@ -20,52 +20,45 @@ C_SHIFT_TENSE = 'shiftTen'
 C_SHIFT_ASPECT = 'shiftAspect'
 C_SIGNAL = 'Signal'
 
+# The mappings below allow us to change the attributes in times and events
+# without having to retrain everything since we can keep the features above
+# unchanged (all that changes is how you get the value)..
 
 EVENT_ATTRIBUTE_MAPPINGS = \
-    [[C_EID, 'eid'], [C_EIID,'eiid'], [C_CLASS,'class'],
-     [C_ASPECT,'aspect'], [C_MOD,'modality'], [C_POL,'polarity'],
-     [C_STRING, 'string'], [C_TENSE, 'tense'], [C_TOE,'timeorevent']]
+    [[C_EID, 'eid'], [C_EIID, 'eiid'], [C_CLASS, 'class'],
+     [C_ASPECT, 'aspect'], [C_MOD, 'modality'], [C_POL, 'polarity'],
+     [C_STRING, 'string'], [C_TENSE, 'tense'], [C_TOE, 'timeorevent']]
 
 TIMEX_ATTRIBUTE_MAPPINGS = \
-    [[C_TID,'tid'], [C_TOE,'timeorevent'], [C_STRING,'string'],
-     [C_FUNINDOC,'functionInDoc'], [C_TEMPFUN,'temporalFunction'],
-     [C_TYPE,'TYPE'], [C_VALUE, 'VAL']]
+    [[C_TID, 'tid'], [C_TOE, 'timeorevent'], [C_STRING, 'string'],
+     [C_FUNINDOC, 'functionInDoc'], [C_TEMPFUN, 'temporalFunction'],
+     [C_TYPE, 'type'], [C_VALUE, 'value']]
 
 
-def create_vectors(xmldoc, ee_file, et_file):
-
+def create_vectors(docelement, ee_file, et_file):
     """New method that takes over the functionality of the old
     Perl script named prepareClassifier."""
 
-    # collect the instance so we can merge in the information when we
-    # find and event.    
-    instances = {}
-    for instance in xmldoc.get_tags(MAKEINSTANCE):
-        eid = instance.attrs.get(EVENTID, None)
-        instances[eid] = instance
-
-    # get object vectors simply by creating a list of all events and times
     object_vectors = []
-    for el in xmldoc:
-        if el.is_opening_tag():
-            if el.tag == EVENT:
-                eid = el.attrs.get(EID, None)
-                object_vectors.append(EventVector(el, instances[eid]))
-            if el.tag == TIMEX:
-                object_vectors.append(TimexVector(el))
+    for event in docelement.tarsqi_tags.find_tags(EVENT):
+        object_vectors.append(EventVector(docelement, event))
+    for timex in docelement.tarsqi_tags.find_tags(TIMEX):
+        object_vectors.append(TimexVector(docelement, timex))
+    for v in object_vectors:
+        print v.as_string()
 
     # mimic what was done in prepareClassifer, even though it seems moronic
-    ee_file = open(ee_file, 'w') 
-    et_file = open(et_file, 'w') 
-    for i in range(0,len(object_vectors)-1):
+    ee_file = open(ee_file, 'w')
+    et_file = open(et_file, 'w')
+    for i in range(0, len(object_vectors) - 1):
         v1 = object_vectors[i]
         v2 = object_vectors[i+1]
         if v1.is_event_vector() and v2.is_event_vector():
-            ee_file.write(EEVector(v1,v2).as_string() + "\n")
+            ee_file.write(EEVector(v1, v2).as_string() + "\n")
         elif v1.is_event_vector() and v2.is_timex_vector():
-            et_file.write(ETVector(v1,v2).as_string() + "\n")
+            et_file.write(ETVector(v1, v2).as_string() + "\n")
         elif v1.is_timex_vector() and v2.is_event_vector():
-            et_file.write(ETVector(v2,v1).as_string() + "\n")
+            et_file.write(ETVector(v2, v1).as_string() + "\n")
 
 
 class Vector:
@@ -76,58 +69,58 @@ class Vector:
     def is_timex_vector(self):
         return False
 
-    def as_string(self, i):
+    def as_string(self, i=0):
         x = []
         for (name, attr) in self.mappings:
-            x.append("%d%s-%s" % (i,name, self.get_value(attr)))
+            x.append("%d%s-%s" % (i, name, self.get_value(attr)))
         return ' '.join(x)
 
 
 class EventVector(Vector):
 
-    def __init__(self, event, instance):
+    def __init__(self, docelement, event):
         self.attrs = event.attrs
-        self.attrs.update(instance.attrs)
-        self.attrs['string'] = event.collect_text_content()
+        self.attrs['string'] = docelement.doc.text(event.begin, event.end)
         self.mappings = EVENT_ATTRIBUTE_MAPPINGS
-        
+
     def is_event_vector(self):
         return True
-    
+
     def get_value(self, attr):
         if attr == 'modality':
-            return self.attrs.get(attr,'NONE')
+            return self.attrs.get(attr, 'NONE')
         elif attr == 'timeorevent':
             return 'event'
         elif attr == 'polarity':
-            if self.attrs.get(attr,'POS') == 'POS':
-                return 'NONE'
-            else:
-                return 'NEG'
-        return self.attrs[attr]
-    
-    
+            # TODO: this is wrong
+            return 'NONE' if self.attrs.get(attr, 'POS') == 'POS' else 'NEG'
+        else:
+            return self.attrs.get(attr, 'no_val')
+
+
 class TimexVector(Vector):
 
-    def __init__(self, timex):
+    def __init__(self, docelement, timex):
         self.attrs = timex.attrs
-        self.attrs['string'] = timex.collect_text_content()
+        print self.attrs
+        self.attrs['string'] = docelement.doc.text(timex.begin, timex.end)
         self.mappings = TIMEX_ATTRIBUTE_MAPPINGS
-        
+
     def is_timex_vector(self):
         return True
-    
+
     def get_value(self, attr):
+        # TODO: this is wrong, but keep it for now
         if attr == 'temporalFunction':
             return 'time'
-        if attr == 'timeorevent':
+        elif attr == 'timeorevent':
             return 'NONE'
         elif attr == 'functionInDoc':
-            return self.attrs.get(attr,'false')
+            return self.attrs.get(attr, 'false')
         elif attr == 'temporalFunction':
-            return self.attrs.get(attr,'time')
-        return self.attrs.get(attr, 'no_val')
-    
+            return self.attrs.get(attr, 'time')
+        else:
+            return self.attrs.get(attr, 'no_val')
 
 
 class EEVector:
@@ -142,7 +135,7 @@ class EEVector:
         1aspect-NONE 1modality-NONE # 1negation-NONE \1string-killing
         1tense-NONE 1timeorevent-event # Signal-NONE shiftAspect-0
         shiftTen-1"""
-    
+
     def __init__(self, event_vector1, event_vector2):
         self.v1 = event_vector1
         self.v2 = event_vector2
@@ -175,4 +168,5 @@ class ETVector:
         self.v2 = timex_vector
 
     def as_string(self):
-        return "UNKNOWN %s %s Signal-NONE" % (self.v1.as_string(0), self.v2.as_string(1))
+        return "UNKNOWN %s %s Signal-NONE" % \
+            (self.v1.as_string(0), self.v2.as_string(1))
--- code/components/classifier/wrapper.py
+++ code/components/classifier/wrapper.py
@@ -4,7 +4,7 @@ Python wrapper around the MaxEnt Classifier
 
 CLASSES
    ClassifierWrapper
-   
+
 """
 
 import os
@@ -12,7 +12,7 @@ import os
 from library.tarsqi_constants import CLASSIFIER
 from library.timeMLspec import TLINK, EIID, TID
 from library.timeMLspec import RELTYPE, EVENT_INSTANCE_ID, TIME_ID
-from library.timeMLspec import RELATED_TO_EVENT_INSTANCE, RELATED_TO_TIME, CONFIDENCE
+from library.timeMLspec import RELATED_TO_EVENT_INSTANCE, RELATED_TO_TIME, ORIGIN, CONFIDENCE
 from utilities import logger
 from components.classifier import vectors
 
@@ -20,47 +20,32 @@ TTK_ROOT = os.environ['TTK_ROOT']
 
 
 class ClassifierWrapper:
-
     """Wraps the maxent link classifier."""
 
-
     def __init__(self, document):
-
         self.component_name = CLASSIFIER
         self.document = document
         self.DIR_CLASSIFIER = os.path.join(TTK_ROOT, 'components', 'classifier')
         self.DIR_DATA = os.path.join(TTK_ROOT, 'data', 'tmp')
-        platform = self.document.getopt_platform()
+        platform = self.document.options.platform
         if platform == 'linux2':
             self.executable = 'mxtest.opt.linux'
         elif platform == 'darwin':
             self.executable = 'mxtest.opt.osx'
-        
-        
+
     def process(self):
-
-        """Retrieve the XmlDocument and hand it to the classifier for processing. Processing will
-        update this slice when tlinks are added."""
-
+        """Retrieve the elements and hand them to the classifier for
+        processing. Processing will update the element's tarsqi_tag
+        repository when tlinks are added."""
         os.chdir(self.DIR_CLASSIFIER)
-        perl = self.document.getopt_perl()
         ee_model = os.path.join('data', 'op.e-e.model')
         et_model = os.path.join('data', 'op.e-t.model')
-
-        #xmldocs = self.document.get_tag_contents_as_xmldocs(self.tag)
-        xmldocs = [self.document.xmldoc]
-        fragment_count = 0
-
-        for xmldoc in xmldocs:
-
-            fragment_count += 1
-            ee_vectors = os.path.join(self.DIR_DATA, "frag%03d.cla.EE" % fragment_count)
-            et_vectors = os.path.join(self.DIR_DATA, "frag%03d.cla.ET" % fragment_count)
+        for (i, element) in enumerate(self.document.elements):
+            ee_vectors = os.path.join(self.DIR_DATA, "frag%03d.cla.EE" % i)
+            et_vectors = os.path.join(self.DIR_DATA, "frag%03d.cla.ET" % i)
             ee_results = ee_vectors + '.REL'
             et_results = et_vectors + '.REL'
-
-            vectors.create_vectors(xmldoc, ee_vectors, et_vectors)
-
+            vectors.create_vectors(element, ee_vectors, et_vectors)
             commands = [
                 "./%s -input %s -model %s -output %s > class.log" %
                 (self.executable, ee_vectors, ee_model, ee_results),
@@ -68,15 +53,13 @@ class ClassifierWrapper:
                 (self.executable, et_vectors, et_model, et_results) ]
             for command in commands:
                 os.system(command)
+            self._add_links(element,
+                            ee_vectors, et_vectors,
+                            ee_results, et_results)
 
-            self._add_links_to_xmldoc(xmldoc, ee_vectors, et_vectors, ee_results, et_results)
-            
- 
-    def _add_links_to_xmldoc(self, xmldoc, ee_vectors, et_vectors, ee_results, et_results):
-
-        """Insert new tlinks into the xmldco using the vectors and the results
+    def _add_links(self, element, ee_vectors, et_vectors, ee_results, et_results):
+        """Insert new tlinks into the element using the vectors and the results
         from the classifier."""
-        
         for (f1, f2) in ((ee_vectors, ee_results), (et_vectors, et_results)):
             vector_file = open(f1)
             classifier_file = open(f2)
@@ -86,40 +69,37 @@ class ClassifierWrapper:
                 id1 = self._get_id('0', attrs, line)
                 id2 = self._get_id('1', attrs, line)
                 (rel, confidence) = self._parse_classifier_line(classifier_line)[0:2]
-                origin = CLASSIFIER + ' ' + confidence
-                xmldoc.add_tlink(rel, id1, id2, origin)
-
+                origin = CLASSIFIER + '-' + confidence
+                id1_attr = TIME_ID if id1.startswith('t') else EVENT_INSTANCE_ID
+                id2_attr = RELATED_TO_TIME if id2.startswith('t') else RELATED_TO_EVENT_INSTANCE
+                attrs = {
+                     RELTYPE: rel,
+                     id1_attr: id1,
+                     id2_attr: id2,
+                     ORIGIN: origin }
+                element.tarsqi_tags.add_tag(TLINK, -1, -1, attrs)
 
     def _parse_vector_string(self, line):
-
-        """Return the attribute dictionaries from the vestor string. """
-        
+        """Return the attribute dictionaries from the vector string. """
         attrs = {}
         for pair in line.split():
             if pair.find('-') > -1:
-                (attr, val) = pair.split('-',1)
+                (attr, val) = pair.split('-', 1)
                 attrs[attr] = val
         return attrs
 
-    
     def _parse_classifier_line(self, line):
-
         """Extract relType, confidence correct/incorrect and correct relation
         from the classifier result line."""
-        
         line = line.strip()
         (rel, confidence, judgment, correct_judgement) = line.split()
         return (rel, confidence, judgment, correct_judgement)
 
-
     def _get_id(self, prefix, attrs, line):
-
         """Get the eiid or tid for the first or second object in the
         vector. The prefix is '0' or '1' and determines which object's
         id is returned."""
-        
         id = attrs.get(prefix+EIID, attrs.get(prefix+TID, None))
         if not id:
             logger.warn("Could not find id in " + line)
         return id
-
