d8349d946e5961b8458b80a60e178f5412fff0de
Marc Verhagen <marc@cs.brandeis.edu>
2016-04-16 16:47:44 -0400

    Added code for S2T and Blinker tests and a few minimal cases.

--- code/testing/run_tests.py
+++ code/testing/run_tests.py
@@ -38,8 +38,8 @@ Examples:
    $ python run_tests.py --evita
    $ python run_tests.py --gutime --evita
 
-An earlier version of this was done with UnitTest, but this way it was easier to
-deal with reading an arbitrary number of tests from a file.
+An earlier version of this was done with UnitTest, but this way made it easier
+to deal with reading an arbitrary number of tests from a file.
 
 """
 
@@ -53,6 +53,8 @@ from cases.test_cases_timex import TIMEX_TESTS
 from cases.test_cases_event import EVENT_TESTS
 from cases.test_cases_slink import ALINK_TESTS
 from cases.test_cases_slink import SLINK_TESTS
+from cases.test_cases_tlink import S2T_TESTS
+from cases.test_cases_tlink import BLINKER_TESTS
 
 
 PASS = 'ok'
@@ -65,7 +67,8 @@ SHOW_ERRORS = False
 GUTIME_PIPELINE = 'PREPROCESSOR,GUTIME'
 EVITA_PIPELINE = 'PREPROCESSOR,EVITA'
 SLINKET_PIPELINE = 'PREPROCESSOR,GUTIME,EVITA,SLINKET'
-#SLINKET_PIPELINE = 'PREPROCESSOR,EVITA,SLINKET'
+S2T_PIPELINE = 'PREPROCESSOR,EVITA,SLINKET,S2T'
+BLINKER_PIPELINE = 'PREPROCESSOR,GUTIME,EVITA,BLINKER'
 
 # this is used when we run all tests so we can print a summary at the end
 SUMMARY = []
@@ -133,11 +136,11 @@ class TarsqiLinkTest(TarsqiTest):
         self.sentence = test_specification[5]
         self.linkname = tag
         self.filename = test_specification[1]
-        self.fsa_rule = test_specification[2]
+        self.rule = test_specification[2]
         self.reltype = test_specification[0]
         self.e1 = test_specification[3]
         self.e2 = test_specification[4]
-        self.name = "%s-%s" % (self.reltype, self.fsa_rule)
+        self.name = "%s-%s" % (self.reltype, self.rule)
         self.find_tag = True
         if len(test_specification) > 6:
             self.find_tag = test_specification[6]
@@ -168,32 +171,63 @@ class TarsqiLinkTest(TarsqiTest):
 class GUTimeTest(TarsqiEntityTest):
     """Test case for GUTime tests. This test is handed the test specifications and
     uses TarsqiEntityTest with the 'TIMEX3' tag and the GUTime pipeline."""
+
     def __init__(self, test_specification):
         TarsqiEntityTest.__init__(self, 'TIMEX3', test_specification)
+
     def run(self):
         TarsqiEntityTest.run(self, GUTIME_PIPELINE)
+
 
 class EvitaTest(TarsqiEntityTest):
     """Test case for GUTime tests. This test is handed the test specifications and
     uses TarsqiEntityTest with the 'EVENT' tag and the Evita pipeline."""
+
     def __init__(self, test_specification):
         TarsqiEntityTest.__init__(self, 'EVENT', test_specification)
+
     def run(self):
         TarsqiEntityTest.run(self, EVITA_PIPELINE)
+
 
 class SlinketTest(TarsqiLinkTest):
     """Test case for Slinket tests. This test is handed the test specifications and
     the link tag and uses TarsqiLinkTest with the Slinket pipeline."""
+
     def __init__(self, tag, test_specification):
         TarsqiLinkTest.__init__(self, tag, test_specification)
+
     def run(self):
         TarsqiLinkTest.run(self, SLINKET_PIPELINE)
+
+
+class S2TTest(TarsqiLinkTest):
+    """Test case for S2T tests. This test is handed the test specifications and
+    the link tag and uses TarsqiLinkTest with the S2T pipeline."""
+
+    def __init__(self, tag, test_specification):
+        TarsqiLinkTest.__init__(self, tag, test_specification)
+
+    def run(self):
+        TarsqiLinkTest.run(self, S2T_PIPELINE)
+
+
+class BlinkerTest(TarsqiLinkTest):
+    """Test case for Blinker tests. This test is handed the test specifications and
+    the link tag and uses TarsqiLinkTest with the Blinker pipeline."""
+
+    def __init__(self, tag, test_specification):
+        TarsqiLinkTest.__init__(self, tag, test_specification)
+
+    def run(self):
+        TarsqiLinkTest.run(self, BLINKER_PIPELINE)
 
 
 def run_pipeline(pipeline, sentence):
     """Run the sentence through the pipeline and return the resulting
     TarsqiDocument. The process_string method does not trap errors."""
     return tarsqi.process_string(sentence, pipeline=pipeline, loglevel=1)
+
 
 def get_tag(td, tag, o1, o2):
     """Return the tag between offsets o1 and o2 if there is one, return None if
@@ -204,29 +238,40 @@ def get_tag(td, tag, o1, o2):
             return t
     return None
 
-def get_link(td, tagname, e1_offsets, e2_offsets, reltype):
+
+def get_link(td, tagname, t1_offsets, t2_offsets, reltype):
     """Return the link tag with the specified tagname (SLINK, ALINK or TLINK),
     relation type and event locations. Return None if there is no such link"""
-    #link_tags = td.elements[0].tarsqi_tags.find_tags(tagname)
-    #event_tags = td.elements[0].tarsqi_tags.find_tags('EVENT')
     link_tags = td.tags.find_tags(tagname)
     event_tags = td.tags.find_tags('EVENT')
-    if not link_tags:
-        return None
+    timex_tags = td.tags.find_tags('TIMEX3')
     for link_tag in link_tags:
-        eiid1 = link_tag.attrs['eventInstanceID']
+        #print link_tag
         if tagname == 'ALINK':
-            eiid2 = link_tag.attrs['relatedToEventInstance']
+            id1 = link_tag.attrs['eventInstanceID']
+            id2 = link_tag.attrs['relatedToEventInstance']
         elif tagname == 'SLINK':
-            eiid2 = link_tag.attrs['subordinatedEventInstance']
+            id1 = link_tag.attrs['eventInstanceID']
+            id2 = link_tag.attrs['subordinatedEventInstance']
+        elif tagname == 'TLINK':
+            id1 = link_tag.attrs.get('eventInstanceID') \
+                  or link_tag.attrs.get('timeID')
+            id2 = link_tag.attrs.get('relatedToEventInstance') \
+                  or link_tag.attrs.get('relatedToTime')
+            #print id1, id2
         relType = link_tag.attrs['relType']
-        e1 = select_event(eiid1, event_tags)
-        e2 = select_event(eiid2, event_tags)        
-        if (e1.begin == e1_offsets[0] and e1.end == e1_offsets[1]
-            and e2.begin == e2_offsets[0] and e2.end == e2_offsets[1]
+        t1 = select_event(id1, event_tags) \
+             if id1.startswith('e') \
+             else select_timex(id1, timex_tags)
+        t2 = select_event(id2, event_tags) \
+             if id2.startswith('e') \
+             else select_timex(id2, timex_tags)
+        if (t1.begin == t1_offsets[0] and t1.end == t1_offsets[1]
+            and t2.begin == t2_offsets[0] and t2.end == t2_offsets[1]
             and relType == reltype):
             return link_tag
     return None
+
 
 def select_event(eiid, event_tags):
     for event_tag in event_tags:
@@ -234,7 +279,13 @@ def select_event(eiid, event_tags):
             return event_tag
     return None
 
-        
+
+def select_timex(tid, timex_tags):
+    for timex_tag in timex_tags:
+        if timex_tag.attrs['tid'] == tid:
+            return timex_tag
+    return None
+
 
 class ModuleTest(object):
 
@@ -261,9 +312,7 @@ class ModuleTest(object):
             results[PASS] += test.results[PASS]
             results[FAIL] += test.results[FAIL]
             results[ERROR] += test.results[ERROR]
-        p = results[PASS]
-        f = results[FAIL]
-        e = results[ERROR]
+        p, f, e = results[PASS], results[FAIL], results[ERROR]
         print "\nTOTALS:  PASS=%s  FAIL=%s  ERROR=%s\n" % (p, f, e)
         SUMMARY.append([self.module_name, p, f, e])
 
@@ -272,17 +321,31 @@ def test_all():
     test_gutime()
     test_evita()
     test_slinket()
+    test_s2t()
+    test_blinker()
     print_summary()
+
 
 def test_gutime():
     ModuleTest('GUTime', GUTimeTest, TIMEX_TESTS).run()
 
+
 def test_evita():
     ModuleTest('Evita', EvitaTest, EVENT_TESTS).run()
+
 
 def test_slinket():
     ModuleTest('Slinket Alink', SlinketTest, ALINK_TESTS, 'ALINK').run()
     ModuleTest('Slinket Slink', SlinketTest, SLINK_TESTS, 'SLINK').run()
+
+
+def test_s2t():
+    ModuleTest('S2T', S2TTest, S2T_TESTS, 'TLINK').run()
+
+
+def test_blinker():
+    ModuleTest('Blinker', BlinkerTest, BLINKER_TESTS, 'TLINK').run()
+
 
 def print_summary():
     print "\nSUMMARY:\n"
@@ -302,18 +365,25 @@ if __name__ == '__main__':
     run_gutime_tests = False
     run_evita_tests = False
     run_slinket_tests = False
+    run_s2t_tests = False
+    run_blinker_tests = False
 
-    opts, args = getopt.getopt(sys.argv[1:], '', ['evita', 'gutime', 'slinket', 'show-errors'])
+    components = ['evita', 'gutime', 'slinket', 's2t', 'blinker']
+    opts, args = getopt.getopt(sys.argv[1:], '', components + ['show-errors'])
     for opt, val in opts:
         if opt == '--show-errors': SHOW_ERRORS = True
         if opt == '--evita': run_evita_tests = True
         if opt == '--gutime': run_gutime_tests = True
         if opt == '--slinket': run_slinket_tests = True
+        if opt == '--s2t': run_s2t_tests = True
+        if opt == '--blinker': run_blinker_tests = True
 
-    if not (run_gutime_tests or run_evita_tests or run_slinket_tests):
+    if not (run_gutime_tests or run_evita_tests
+            or run_slinket_tests or run_s2t_tests or run_blinker_tests):
         test_all()
     else:
         if run_gutime_tests: test_gutime()
         if run_evita_tests: test_evita()
         if run_slinket_tests: test_slinket()
-        
+        if run_s2t_tests: test_s2t()
+        if run_blinker_tests: test_blinker()
